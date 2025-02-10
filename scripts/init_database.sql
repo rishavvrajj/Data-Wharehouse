bfdc3731d96c011c6416e65819fadfff8e52726f
rishav raj <rishav5raj3@gmail.com>
2025-02-11 04:09:15 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,14 @@
+USE master;
+Go
+-- Create Database 'DataWarehouse'
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+-- Creating a schema
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA Silver;
+GO
+CREATE SCHEMA gold;
+GO
