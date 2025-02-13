14c4ec6746ceb57034f747d126b5d87837edbd0c
rishav raj <rishav5raj3@gmail.com>
2025-02-13 22:17:47 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,14 +1,27 @@
+-- Switch to the 'master' database
 USE master;
-Go
--- Create Database 'DataWarehouse'
+GO
+
+-- Create the DataWarehouse database
 CREATE DATABASE DataWarehouse;
+GO
 
+-- Switch to the 'DataWarehouse' database
 USE DataWarehouse;
+GO
 
--- Creating a schema
+-- *******************************
+-- SCHEMA CREATION
+-- *******************************
+
+-- Create 'bronze' schema for raw or untransformed data
 CREATE SCHEMA bronze;
 GO
+
+-- Create 'silver' schema for cleaned and transformed data
 CREATE SCHEMA Silver;
 GO
+
+-- Create 'gold' schema for aggregated and refined data
 CREATE SCHEMA gold;
 GO
